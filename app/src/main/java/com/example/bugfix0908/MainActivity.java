145966bf66491a76e5f3757da7cf4ad31694a9eb
kirannep <kiran.nepali@hotmail.com>
2019-08-12 21:16:52 +0100

    Bug fix by Kiran

--- app/src/main/java/com/example/bugfix0908/MainActivity.java
+++ app/src/main/java/com/example/bugfix0908/MainActivity.java
@@ -9,21 +9,22 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-private Button btnGoToActivity;
+    private Button btnGoToActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
+        setContentView(R.layout.activity_main);
+        btnGoToActivity = findViewById(R.id.btn_go_to_activity);
         btnGoToActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                goToNextActivity();
             }
         });
     }
 
     private void goToNextActivity(){
         Intent intent = new Intent(this,SecondActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/bugfix0908/SecondActivity.java
+++ app/src/main/java/com/example/bugfix0908/SecondActivity.java
@@ -2,13 +2,18 @@ package com.example.bugfix0908;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
-
+    private TextView tv_secondActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        tv_secondActivity = findViewById(R.id.tv_secondActivity);
+        tv_secondActivity.setVisibility(View.VISIBLE);
     }
 }
